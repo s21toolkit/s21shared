13e2a934a9b4ccf4a380b4e803f9cf6e2fbf9d75
bgenia <xbgenia@gmail.com>
2024-05-21 00:02:40 +0300

    feat: add typings for tsup configurations

---
+++ tsup/configurations.d.ts
@@ -0,0 +1,13 @@
+import type { Options } from "tsup"
+
+export declare namespace configs {
+	export const base: Options
+	export const nodeBase: Options
+	export const browserBase: Options
+
+	export const library: Options
+	export const nodeLibrary: Options
+	export const browserLibrary: Options
+
+	export const cli: Options
+}
